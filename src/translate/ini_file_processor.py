f51d97f52a63c363d65da5eb6f6b6ee6a78d8eca
mvoof <137293673+mvoof@users.noreply.github.com>
2025-05-20 20:32:10 +0500

    translate untranslated values

--- src/translate/ini_file_processor.py
+++ src/translate/ini_file_processor.py
@@ -32,8 +32,9 @@ class IniFileProcessor:
         self,
         key: str,
         value: str,
-        translated_lines: INIDataType,
         exclude_keys: ExcludeKeysType,
+        missing_keys: Set[str],
+        untranslated_keys: Set[str],
     ) -> bool:
         """
         Checks if a key-value pair should be translated.
@@ -41,13 +42,17 @@ class IniFileProcessor:
         Args:
             key (str): The key to check.
             value (str): The value to check.
-            translated_lines (INIDataType): The translated lines.
             exclude_keys (ExcludeKeysType): The keys to exclude from translation.
+            missing_keys (Set[str]): Keys present in input but not in output.
+            untranslated_keys (Set[str]): Keys with untranslated values.
 
         Returns:
             bool: True if the key-value pair should be translated, False otherwise.
         """
-        return key not in translated_lines and key not in exclude_keys and bool(value)
+        if key in exclude_keys or not value:
+            return False
+
+        return key in missing_keys or key in untranslated_keys
 
     def process_line(
         self,
@@ -56,6 +61,8 @@ class IniFileProcessor:
         translator: TranslatorCallableType,
         translated_lines: INIDataType,
         exclude_keys: ExcludeKeysType,
+        missing_keys: Set[str],
+        untranslated_keys: Set[str],
     ) -> str:
         """
         Processes a line by translating its value if necessary.
@@ -66,11 +73,15 @@ class IniFileProcessor:
             translator (TranslatorCallableType): A callable for translating the text.
             translated_lines (INIDataType): A dictionary containing already translated lines.
             exclude_keys (ExcludeKeysType): A tuple of keys to exclude from translation.
+            missing_keys (Set[str]): Keys present in input but not in output.
+            keys_needing_translation (Set[str]): Keys with untranslated values.
 
         Returns:
             str: The processed line in the format 'key=value'.
         """
-        if self._should_translate(key, value, translated_lines, exclude_keys):
+        if self._should_translate(
+            key, value, exclude_keys, missing_keys, untranslated_keys
+        ):
             translated_value: str = self.text_processor.translate_text(
                 value, translator
             )
@@ -119,29 +130,47 @@ class IniFileProcessor:
         self,
         input_items: INIDataType,
         output_items: INIDataType,
-    ) -> Tuple[Set[str], Set[str]]:
+    ) -> Tuple[Set[str], Set[str], Set[str]]:
         """
-        Compares the keys of two dictionaries and returns two sets:
+        Compares keys of input and output dictionaries:
         1. missing_keys: The set of keys present in input_items but not in output_items.
         2. obsolete_keys: The set of keys present in output_items but not in input_items.
+        3. untranslated_keys: The set of keys present in both dictionaries with identical values.
 
         Args:
             input_items (INIDataType): The dictionary of input items.
             output_items (INIDataType): The dictionary of output items.
 
         Returns:
-            Tuple[Set[str], Set[str]]: A tuple containing the missing and obsolete keys.
+            Tuple[Set[str], Set[str], Set[str]]: A tuple containing:
+                missing_keys: Keys present in input but not in output.
+                obsolete_keys: Keys present in output but not in input.
+                untranslated_keys: Keys present in both with identical values.
         """
         input_keys: Set[str] = set(input_items.keys())
         output_keys: Set[str] = set(output_items.keys())
+        exclude_keys: ExcludeKeysType = self.config.translation_config.exclude_keys
 
         missing_keys: Set[str] = input_keys - output_keys
         obsolete_keys: Set[str] = output_keys - input_keys
+        untranslated_keys: Set[str] = {
+            key
+            for key in input_items
+            if (
+                key in output_items
+                and input_items[key] == output_items[key]
+                and key not in exclude_keys
+                and input_items[key]
+            )
+        }
 
         self.logger.info(f"Found {len(missing_keys)} new keys for translation")
         self.logger.info(f"Found {len(obsolete_keys)} obsolete keys to remove")
+        self.logger.info(
+            f"Found {len(untranslated_keys)} keys with not translated value"
+        )
 
-        return missing_keys, obsolete_keys
+        return missing_keys, obsolete_keys, untranslated_keys
 
     def translate_file(
         self,
@@ -154,7 +183,13 @@ class IniFileProcessor:
         input_items, output_items = self._read_files(
             input_translation_file, output_translation_file
         )
-        _, obsolete_keys = self._compare_keys(input_items, output_items)
+        missing_keys, obsolete_keys, untranslated_keys = self._compare_keys(
+            input_items, output_items
+        )
+
+        if not missing_keys and not obsolete_keys and not untranslated_keys:
+            self.logger.info("No changes detected, skipping translation")
+            return
 
         with BufferedFileWriter(
             output_translation_file,
@@ -173,6 +208,8 @@ class IniFileProcessor:
                     translator,
                     output_items,
                     self.config.translation_config.exclude_keys,
+                    missing_keys,
+                    untranslated_keys,
                 )
 
                 writer.write(translated_line)
