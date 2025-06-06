f062d1964097aa5ad76065ea20014470a7449ebf
mvoof <mvoof@proton.me>
2025-06-06 13:36:00 +0500

    split path config to training and translation

--- src/config/paths.py
+++ src/config/paths.py
@@ -1,43 +1,71 @@
 from dataclasses import dataclass, field
 from pathlib import Path
-from typing import Dict, List, cast
+from typing import List, Optional
 
 from src.type_defs import IniFilePathsType, JsonFilePathsType
 
 
 @dataclass
-class PathConfig:
-    input_ini_file: str | None = None
-    original_ini_file: str = "global_original.ini"
-    pre_translated_ini_file_for_training: str = "global_pre_translated.ini"
-
+class BasePathConfig:
     base_dir: Path = field(init=False)
     data_dir: Path = field(init=False)
     models_dir: Path = field(init=False)
-    translation_dir: Path = field(init=False)
     logging_dir: Path = field(init=False)
-    input_ini_file_path: Path = field(init=False)
-    ini_files_for_training: IniFilePathsType = field(init=False)
-    json_files: JsonFilePathsType = field(init=False)
 
     def __post_init__(self) -> None:
         self.base_dir = Path(__file__).parent.parent.parent
         self.base_dir = self.base_dir.relative_to(Path.cwd())
         self.data_dir = self.base_dir / "data"
         self.models_dir = self.base_dir / "models"
-        self.translation_dir = self.data_dir / "translation_results"
         self.logging_dir = self.base_dir / "logs"
 
         self.data_dir.mkdir(parents=True, exist_ok=True)
 
-        if self.input_ini_file:
-            self.input_ini_file_path = Path(self.input_ini_file)
-        else:
-            self.input_ini_file_path = self.data_dir / self.original_ini_file
 
-        self.ini_files_for_training = IniFilePathsType(
-            original=self.data_dir / self.original_ini_file,
-            translated=self.data_dir / self.pre_translated_ini_file_for_training,
+@dataclass
+class TranslationPathConfig(BasePathConfig):
+    input_file: Optional[str] = None
+    translation_dir: Path = field(init=False)
+    input_file_path: Optional[Path] = field(init=False)
+
+    def __post_init__(self) -> None:
+        super().__post_init__()
+
+        self.translation_dir = self.data_dir / "translation_results"
+
+        # Initialize input_file_path
+        if self.input_file is None:
+            self.input_file_path = None
+        else:
+            self.input_file_path = Path(self.input_file)
+
+            if not self.input_file_path.exists():
+                raise FileNotFoundError(
+                    f"Input file does not exist: {self.input_file_path}"
+                )
+
+    def check_input_file_exists(self) -> None:
+        if self.input_file_path is None or not self.input_file_path.exists():
+            raise FileNotFoundError(
+                f"The input INI file does not exist: {self.input_file_path}"
+            )
+
+
+@dataclass
+class TrainingPathConfig(BasePathConfig):
+    training_source_file: str = "global_original.ini"
+    pre_translated_file: str = "global_pre_translated.ini"
+    ini_files: IniFilePathsType = field(init=False)
+    json_files: JsonFilePathsType = field(init=False)
+
+    def __post_init__(self) -> None:
+        super().__post_init__()
+
+        self.ini_files = IniFilePathsType(
+            source=self.data_dir
+            / self.training_source_file,  # Default for translation if needed
+            translated=self.data_dir / self.pre_translated_file,
+            training_source=self.data_dir / self.training_source_file,
         )
 
         self.json_files = JsonFilePathsType(
@@ -50,19 +78,15 @@ class PathConfig:
     def data_files_exist(self) -> bool:
         return self.json_files["train"].exists() and self.json_files["test"].exists()
 
-    def check_ini_files_for_training_exist(self) -> None:
-        ini_values: List[Path] = list(
-            cast(Dict[str, Path], self.ini_files_for_training).values()
-        )
+    def check_ini_files_exist(self) -> None:
+        ini_values: List[Path] = [
+            self.ini_files["source"],
+            self.ini_files["translated"],
+            self.ini_files["training_source"],
+        ]
         missing_files: List[Path] = [path for path in ini_values if not path.exists()]
 
         if missing_files:
             raise FileNotFoundError(
                 f"The following necessary INI files are missing: {', '.join(map(str, missing_files))}"
-            )
-        
-    def check_input_ini_file_exists(self) -> None:
-        if not self.input_ini_file_path.exists():
-            raise FileNotFoundError(
-                f"The input INI file does not exist: {self.input_ini_file_path}"
             )
