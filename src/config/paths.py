5e3d59ff3c85ffa8704a31988af1a900a6caa5ff
mvoof <mvoof@proton.me>
2025-08-08 23:05:55 +0500

    add ner path config

--- src/config/paths.py
+++ src/config/paths.py
@@ -2,13 +2,17 @@ from dataclasses import dataclass, field
 from pathlib import Path
 from typing import List, Optional
 
-from src.type_defs import IniFilePathsType, JsonFilePathsType
+from src.type_defs import (
+    IniFilePathsType,
+    LangCode,
+    JsonTrainedDataFilePathsType,
+)
 
 
 @dataclass
 class BasePathConfig:
-    default_source_file: str = "global_original.ini"
-    pre_translated_file: str = "global_pre_translated.ini"
+    default_source_file: str = field(default="global_original.ini", init=False)
+    pre_translated_file: str = field(default="global_pre_translated.ini", init=False)
 
     base_dir: Path = field(init=False)
     data_dir: Path = field(init=False)
@@ -19,14 +23,47 @@ class BasePathConfig:
         self.base_dir = Path(__file__).parent.parent.parent
         self.base_dir = self.base_dir.relative_to(Path.cwd())
         self.data_dir = self.base_dir / "data"
+
         self.models_dir = self.base_dir / "models"
         self.logging_dir = self.base_dir / "logs"
 
         self.data_dir.mkdir(parents=True, exist_ok=True)
 
+    def get_raw_data_dir_by_lang(self, lang: LangCode) -> Path:
+        raw_dir = self.data_dir / "raw" / lang.value
+        raw_dir.mkdir(parents=True, exist_ok=True)
+
+        return raw_dir
+
+    def get_training_data_dir_by_lang(
+        self, src_lang: LangCode, tgt_lang: LangCode
+    ) -> Path:
+        training_dir = self.data_dir / "training" / f"{src_lang.value}-{tgt_lang.value}"
+        training_dir.mkdir(parents=True, exist_ok=True)
+
+        return training_dir
+
+    def get_translation_results_dir_by_lang(
+        self, src_lang: LangCode, tgt_lang: LangCode
+    ) -> Path:
+        translation_dir = (
+            self.data_dir / "translation_results" / f"{src_lang.value}-{tgt_lang.value}"
+        )
+        translation_dir.mkdir(parents=True, exist_ok=True)
+
+        return translation_dir
+
+    def get_ner_data_dir(self) -> Path:
+        ner_dir = self.data_dir / "ner"
+        ner_dir.mkdir(parents=True, exist_ok=True)
+
+        return ner_dir
+
 
 @dataclass
 class TranslationPathConfig(BasePathConfig):
+    src_lang: LangCode = LangCode.EN
+    tgt_lang: LangCode = LangCode.RU
     input_file: Optional[str] = None
     translation_dir: Path = field(init=False)
     input_file_path: Path = field(init=False)
@@ -34,11 +71,15 @@ class TranslationPathConfig(BasePathConfig):
     def __post_init__(self) -> None:
         super().__post_init__()
 
-        self.translation_dir = self.data_dir / "translation_results"
+        self.translation_dir = self.get_translation_results_dir_by_lang(
+            self.src_lang, self.tgt_lang
+        )
 
         # Initialize input_file_path
         if self.input_file is None:
-            self.input_file_path = self.data_dir / self.default_source_file
+            self.input_file_path = (
+                self.get_raw_data_dir_by_lang(self.src_lang) / self.default_source_file
+            )
         else:
             self.input_file_path = Path(self.input_file)
 
@@ -56,26 +97,40 @@ class TranslationPathConfig(BasePathConfig):
 
 @dataclass
 class TrainingPathConfig(BasePathConfig):
+    src_lang: LangCode = LangCode.EN
+    tgt_lang: LangCode = LangCode.RU
+
+    raw_dataset: Path = field(init=False)
+    cleaned_dataset: Path = field(init=False)
+
     ini_files: IniFilePathsType = field(init=False)
-    json_files: JsonFilePathsType = field(init=False)
+    trained_data_files: JsonTrainedDataFilePathsType = field(init=False)
 
     def __post_init__(self) -> None:
         super().__post_init__()
 
+        training_dir = self.get_training_data_dir_by_lang(self.src_lang, self.tgt_lang)
+
         self.ini_files = IniFilePathsType(
-            original=self.data_dir / self.default_source_file,
-            translated=self.data_dir / self.pre_translated_file,
+            original=self.get_raw_data_dir_by_lang(self.src_lang)
+            / self.default_source_file,
+            translated=self.get_raw_data_dir_by_lang(self.tgt_lang)
+            / self.pre_translated_file,
         )
 
-        self.json_files = JsonFilePathsType(
-            data=self.data_dir / "data.json",
-            cleaned=self.data_dir / "cleaned_data.json",
-            train=self.data_dir / "train.json",
-            test=self.data_dir / "test.json",
+        self.raw_dataset = training_dir / "data.json"
+        self.cleaned_dataset = training_dir / "cleaned_data.json"
+
+        self.trained_data_files = JsonTrainedDataFilePathsType(
+            train=training_dir / "train.json",
+            test=training_dir / "test.json",
         )
 
     def data_files_exist(self) -> bool:
-        return self.json_files["train"].exists() and self.json_files["test"].exists()
+        return (
+            self.trained_data_files["train"].exists()
+            and self.trained_data_files["test"].exists()
+        )
 
     def check_ini_files_exist(self) -> None:
         ini_values: List[Path] = [
@@ -88,3 +143,67 @@ class TrainingPathConfig(BasePathConfig):
             raise FileNotFoundError(
                 f"The following necessary INI files are missing: {', '.join(map(str, missing_files))}"
             )
+
+
+@dataclass
+class NERPathConfig(BasePathConfig):
+    input_file: Optional[str] = None
+
+    input_file_path: Path = field(init=False)
+
+    extracted_ner_data_path: Path = field(init=False)
+
+    temp_streamlit_data_path: Path = field(init=False)
+    corrected_streamlit_data_path: Path = field(init=False)
+
+    bio_ner_dataset_path: Path = field(init=False)
+    trained_data_files: JsonTrainedDataFilePathsType = field(init=False)
+
+    output_dir: Path = field(init=False)
+    logging_dir: Path = field(init=False)
+
+    def __post_init__(self) -> None:
+        super().__post_init__()
+
+        raw_dir = self.get_raw_data_dir_by_lang(
+            LangCode.EN
+        )  # always use original English file
+        ner_data_dir = self.get_ner_data_dir()
+
+        self.extracted_ner_data_path = ner_data_dir / "ner_unannotated.json"
+
+        self.temp_streamlit_data_path = ner_data_dir / "temp_corrected.json"
+        self.corrected_streamlit_data_path = ner_data_dir / "dataset_corrected.json"
+
+        self.bio_ner_dataset_path = ner_data_dir / "dataset_bio.json"
+
+        self.trained_data_files = JsonTrainedDataFilePathsType(
+            train=ner_data_dir / "train.json",
+            test=ner_data_dir / "test.json",
+        )
+
+        self.output_dir = self.models_dir / "ner_output"
+        self.logging_dir = self.logging_dir / "ner_logs"
+
+        # Initialize input_file_path
+        if self.input_file is None:
+            self.input_file_path = raw_dir / self.default_source_file
+        else:
+            self.input_file_path = Path(self.input_file)
+
+            if not self.input_file_path.exists():
+                raise FileNotFoundError(
+                    f"Input file does not exist: {self.input_file_path}"
+                )
+
+    def check_input_file_exists(self) -> None:
+        if not self.input_file_path.exists():
+            raise FileNotFoundError(
+                f"The input INI file does not exist: {self.input_file_path}"
+            )
+
+    def data_files_exist(self) -> bool:
+        return (
+            self.trained_data_files["train"].exists()
+            and self.trained_data_files["test"].exists()
+        )
