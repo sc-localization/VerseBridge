2cc0411153e2b8c46f7eb45b458986f0fb898a3a
mvoof <137293673+mvoof@users.noreply.github.com>
2025-08-13 22:13:17 +0500

    fix  model name type in ModelConfig

--- src/config/models.py
+++ src/config/models.py
@@ -2,14 +2,14 @@ from dataclasses import dataclass, field
 from pathlib import Path
 
 
-from src.type_defs import ModelNameType, LastCheckpointPathType
+from src.type_defs import TranslationModelNameType, LastCheckpointPathType
 from .paths import TrainingPathConfig
 
 
 @dataclass
 class ModelConfig:
     path_config: TrainingPathConfig
-    model_name: ModelNameType = "facebook/nllb-200-distilled-1.3B"
+    model_name: TranslationModelNameType = "facebook/nllb-200-distilled-1.3B"
     result_path: Path = field(init=False)
     checkpoints_path: Path = field(init=False)
     last_checkpoint: LastCheckpointPathType = field(init=False, default=None)
