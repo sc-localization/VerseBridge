8a5df2319ca6df8d122469cd8e72dfd7155f2028
mvoof <mvoof@proton.me>
2025-09-06 22:41:46 +0500

    refactor: get different max_length for training and translation

--- src/config/dataset.py
+++ src/config/dataset.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-from src.type_defs import TokenizerConfigType
+from src.type_defs import TokenizerOptionsType, TokenizerConfigType
 
 
 @dataclass
@@ -9,10 +9,17 @@ class DatasetConfig:
     max_training_length: int = 128
     data_split_ratio: float = 0.9
 
-    def to_dict(self) -> TokenizerConfigType:
+    @classmethod
+    def _to_dict(cls) -> TokenizerOptionsType:
         return {
             "return_tensors": "pt",
             "padding": True,
             "truncation": True,
+        }
+
+    @property
+    def translation_dict(self) -> TokenizerConfigType:
+        return {
+            **self._to_dict(),
             "max_length": int(self.max_model_length),
         }
